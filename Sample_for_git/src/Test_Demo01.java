a596f2dc15ab92f1f5e26c3a1f494859c47e055b
karunakrishna <karuna.krishna@exterro.com>
2018-12-20 10:56:02 +0530

    Commit01MML

---
+++ Sample_for_git/src/Test_Demo01.java
@@ -0,0 +1,13 @@
+
+public class Test_Demo01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String [] name = {"Apple","moon","sun","Mango"};
+		for(String dest : name){
+			System.out.println(dest);
+		}
+
+	}
+
+}
